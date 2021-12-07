4f5068b36f1048fac36e8bf53563663a5316202e
Brenda-Machado <87547436+Brenda-Machado@users.noreply.github.com>
2021-12-07 13:28:25 -0300

    Update 4.isp.py

--- 4.isp.py
+++ 4.isp.py
@@ -1,7 +1,7 @@
 """
 Interface Segregation Principle
 Crie interfaces que são específicas. Clientes não devem depender de interfaces que eles não usarão
-"""
+
 from abc import ABC
 
 class IJanela(ABC):
@@ -42,5 +42,37 @@ class JanelaSemMenu(IJanela):
     
     def fechar(self):
         pass
+"""   
+#Forma de resolver: criar a interface Tamanho(métodos maximizar e minimizar) e a interface Menu(mostrar_menu e fechar)
 
+from abc import ABC
+
+class ITamanho(ABC):
+    def maximizar(self):
+        raise NotImplementedError
+    
+    def minimizar(self):
+        raise NotImplementedError
+        
+class IMenu(ABC):
+    def mostrar_menu(self):
+        raise NotImplementedError
+        
+    def fechar(self):
+        raise NotImplementedError
+
+class JanelaTamanhoFixo(IMenu):
+    
+    def mostrar_menu(self):
+        pass
+    
+    def fechar(self):
+        pass
+
+class JanelaSemMenu(ITamanho):
+    def maximizar(self):
+        pass
+
+    def minimizar(self):
+        pass
 
